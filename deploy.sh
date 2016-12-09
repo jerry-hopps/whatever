66cf3ba92fc1394c79e03c448035648637b66e00
Tony Shi <tuo.shi@oracle.com>
2016-12-09 22:44:42 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+branch=$1
+if [ x$1 == x ]; then
+  branch='master'
+fi
+
+echo $branch
+sudo service tomcat7 stop
+mvn clean
+git stash
+git fetch
+git reset --hard $0
+git stash apply
+mvn package
+sudo service tomcat7 start
