4ce4015025588f54f9cf94c91f326d4dcf75d866
Tony Shi <tuo.shi@oracle.com>
2016-12-12 16:49:00 +0800

    set rememberMe=false

--- src/main/java/net/nemo/whatever/controller/UserController.java
+++ src/main/java/net/nemo/whatever/controller/UserController.java
@@ -151,7 +151,7 @@ public class UserController {
 		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
 		Subject currentUser = SecurityUtils.getSubject();
 
-		token.setRememberMe(true);
+		token.setRememberMe(false);
 		currentUser.login(token);
 		User loginUser = userService.findByEmail(username);
 		currentUser.getSession().setAttribute("currentUser", loginUser);
